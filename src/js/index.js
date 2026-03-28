b56a01937201b215e79cb67181d1d9fb1a9c2f58
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-03-28 19:21:04 -0300

    feat: add basic html css js structure

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+const cardsContainer = document.querySelector('.cards-container')
+
+async function getData() {
+    file = '../../tarot_pt_json.json'
+    const response = await fetch(file);
+    const data = await response.json();
+    return data;
+}
+
+getData().then(data => {
+    const cards = data.cartas;
+    console.log(cards[0]);
+
+    const arcanosMaiores = cards.slice(0, 22);
+
+    cardsContainer.innerHTML = arcanosMaiores.map((card) => {
+        return `
+        <div class="card">
+            <img src="../${card.imagem}" alt="Carta do Tartot: ${card.nome}">
+            <h2>${card.nome}</h2>
+            <p>${card.descricaoCurta}</p>
+        </div>
+        `
+    }).join('');
+})
